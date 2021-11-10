3261dad39c35c5c5d201116b20297fc88f5eabe1
Azri Kahar <azrikahar@outlook.com>
2021-11-10 17:03:28 +0800

    fix public url env

--- directus.config.js
+++ directus.config.js
@@ -11,7 +11,7 @@ module.exports = function (env) {
         PORT: env.PORT,
 
         // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
-        PUBLIC_URL: `https://${RAILWAY_STATIC_URL}`,
+        PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
 
         // Database variables from Railway PostgreSQL Plugin
         // Reference: https://docs.railway.app/plugins/postgresql
